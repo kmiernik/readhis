b0c227d5fa66ca962822ec68bc1250824bb56d5d
Krzysztof Miernik <k.a.miernik@gmail.com>
2012-01-09 12:17:15 -0500

    Further work on readhis program.

--- readhis.cpp
+++ readhis.cpp
@@ -1,21 +1,38 @@
 #include <cstdlib>
 #include <getopt.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
     
 static struct option long_options[] = {
-    {"list",  no_argument, 0,       'l'},
-    {"listZ", no_argument, 0,       'L'},
+    {"id",    required_argument, 0, 'i'},
     {"gx",    required_argument, 0, 'x'},
     {"gy",    required_argument, 0, 'y'},
     {"bg",    required_argument, 0, 'b'},
     {"sbg",   required_argument, 0, 's'},
+    {"info",  no_argument, 0,       'I'},
+    {"list",  no_argument, 0,       'l'},
+    {"listZ", no_argument, 0,       'L'},
     {"zero",  required_argument, 0, 'z'},
     {0, 0, 0, 0}
 };
 
-class Flags {
+class Options {
     public:
         unsigned getHisId () { return hisId_; }
-        void setHisId (unsigned const hisId) { hisId_ = hisId; }
+        bool setHisId (unsigned const hisId) {
+            if (hisId > 0 && hisId < 10000) {
+                hisId_ = hisId;
+                isIdSet_ = true;
+                return true;
+            } else {
+                isIdSet_ = false;
+                return false;
+            }
+        }
+        bool getisIdSet() { return isIdSet_; }
 
         bool getListMode() { return isListMode_; }
         void setListMode (const bool b) { 
@@ -46,8 +63,12 @@ class Flags {
             if (g0 > g1)
                 return false;
             isGx_ = isGx;
-            g0_ = g0;
-            g1_ = g1;
+            if (isGx) {
+                isGy_ = false;
+                g0_ = g0;
+                g1_ = g1;
+            }
+            return true;
         }
 
         bool getGy() { return isGy_; }
@@ -55,19 +76,55 @@ class Flags {
             if (g0 > g1)
                 return false;
             isGy_ = isGy;
+            if (isGy) {
+                isGx_ = false;
+                g0_ = g0;
+                g1_ = g1;
+            }
             g0_ = g0;
             g1_ = g1;
+            return true;
+        }
+
+        bool getBg() { return isBg_; }
+        bool setBg (const bool isBg, const unsigned b0, const unsigned b1) {
+            if (b0 > b1)
+                return false;
+            isBg_ = isBg;
+            if (isBg) {
+                isSBg_ = false;
+                b0_ = b0;
+                b1_ = b1;
+            }
+            return true;
         }
         
-        Flags() {
+        bool getSBg() { return isSBg_; }
+        bool setSBg (const bool isSBg, const unsigned b0, const unsigned b1,
+                                       const unsigned b2, const unsigned b3) {
+            if ( !(b3 > b2 && b2 > b1 && b1 > b0) )
+                return false;
+            isSBg_ = isSBg;
+            if (isSBg) {
+                isBg_ = false;
+                b0_ = b0;
+                b1_ = b1;
+                b2_ = b2;
+                b3_ = b3;
+            }
+            return true;
+        }
+
+        Options() {
             hisId_ = 0;
+            isIdSet_ = false;
             isListMode_ = false;
             isListModeZ_ = false;
             isZeroSup_ = false;
             isGx_ = false;
             isGy_ = false;
             isBg_ = false;
-            isSbg_ = false;
+            isSBg_ = false;
             g0_ = 0;
             g1_ = 0;
             b0_ = 0;
@@ -77,13 +134,14 @@ class Flags {
         }
     private:
         unsigned hisId_;
+        bool isIdSet_;
         bool isListMode_;
         bool isListModeZ_;
         bool isZeroSup_;
         bool isGx_;
         bool isGy_;
         bool isBg_;
-        bool isSbg_;
+        bool isSBg_;
         unsigned g0_;
         unsigned g1_;
         unsigned b0_;
@@ -91,78 +149,180 @@ class Flags {
         unsigned b2_;
         unsigned b3_;
 };
+
+enum Status {
+    ok,
+    warning,
+    error
+};
+
+Status parseMultiArgs (char* arguments, int* argsArray, unsigned requiredArgs = 0) {
+    char* tokens;
+    tokens = strtok(arguments, ",");
+
+    unsigned numberOfTokens = 0;
+    while (tokens != 0) {
+        if (numberOfTokens < requiredArgs)
+            argsArray[numberOfTokens] = atoi(tokens);
+        else
+            return warning;
+        tokens = strtok(0, ",");
+        numberOfTokens++;
+    }    
+    if (numberOfTokens == requiredArgs)
+        return ok;
+    else
+        return error;
+}
      
 int main (int argc, char* argv[]) {
-    int c;
-       
-     
-       while (true)
-         {
-           /* getopt_long stores the option index here. */
-           int option_index = 0;
-     
-           c = getopt_long (argc, argv, "abc:d:f:",
-                            long_options, &option_index);
-     
-           /* Detect the end of the options. */
-           if (c == -1)
-             break;
-     
-           switch (c)
-             {
-             case 0:
-               /* If this option set a flag, do nothing else now. */
-               if (long_options[option_index].flag != 0)
-                 break;
-               printf ("option %s", long_options[option_index].name);
-               if (optarg)
-                 printf (" with arg %s", optarg);
-               printf ("\n");
-               break;
-     
-             case 'a':
-               puts ("option -a\n");
-               break;
-     
-             case 'b':
-               puts ("option -b\n");
-               break;
-     
-             case 'c':
-               printf ("option -c with value `%s'\n", optarg);
-               break;
-     
-             case 'd':
-               printf ("option -d with value `%s'\n", optarg);
-               break;
-     
-             case 'f':
-               printf ("option -f with value `%s'\n", optarg);
-               break;
-     
-             case '?':
-               /* getopt_long already printed an error message. */
-               break;
-     
-             default:
-               abort ();
-             }
-         }
-     
-       /* Instead of reporting ‘--verbose’
-          and ‘--brief’ as they are encountered,
-          we report the final status resulting from them. */
-       if (verbose_flag)
-         puts ("verbose flag is set");
-     
-       /* Print any remaining command line arguments (not options). */
-       if (optind < argc)
-         {
-           printf ("non-option ARGV-elements: ");
-           while (optind < argc)
-             printf ("%s ", argv[optind++]);
-           putchar ('\n');
-         }
-     
-       exit (0);
+
+    int c = 0;
+
+    Options* options = new Options();
+
+    while (true) {
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
+
+        c = getopt_long (argc, argv, "i:x:y:b:s:zIlL",
+                        long_options, &option_index);
+
+        /* Detect the end of the options. */
+        if (c == -1)
+            break;
+
+        switch (c) {
+
+            case 'I': {
+                cout << "Option -I" << endl;
+                break;
+            }
+
+            case 'i': {
+                cout << "Option -i " << endl;
+                int hisId = atoi(optarg);
+                if ( !options->setHisId(hisId) ) {
+                    cout << "Wrong or missing hisId " << endl
+                    abort();
+                }
+                cout << "Id: " << options->getHisId() << endl;
+                break;
+            }
+
+            case 'l': {
+                cout << "Option -l " << endl;
+                options->setListMode(true);
+                break;
+            }
+
+            case 'L':
+                cout << "Option -L " << endl;
+                options->setListModeZ(true);
+                break;
+
+            case 'x': {
+                int a[2] = {0};
+                Status status = parseMultiArgs(optarg, a, 2);
+                switch (status){
+                    case ok:
+                        cout << "Option -x with values " << a[0] << " and " << a[1] << endl;
+                        if ( !(options->setGx(true, a[0], a[1])) )
+                            status = error;
+                        break;
+                    case warning:
+                        cout << "Warning: option -x with values " << a[0] << " and " << a[1] << endl;
+                        if ( !(options->setGx(true, a[0], a[1])) )
+                            status = error;
+                        break;
+                    case error:
+                        cout << "Error: option -x with values " << a[0] << " and " << a[1] << endl;
+                }
+                break;
+            }
+
+            case 'y': {
+                int a[2] = {0};
+                Status status = parseMultiArgs(optarg, a, 2);
+                switch (status){
+                    case ok:
+                        cout << "Option -y with values " << a[0] << " and " << a[1] << endl;
+                        if ( !(options->setGy(true, a[0], a[1])) )
+                            status = error;
+                        break;
+                    case warning:
+                        cout << "Warning: option -y with values " << optarg << endl;
+                        if ( !(options->setGy(true, a[0], a[1])) )
+                            status = error;
+                        break;
+                    case error:
+                        cout << "Error: option -y with values " << optarg << endl;
+                }
+                break;
+            }
+
+            case 'b': {
+                int a[2] = {0};
+                Status status = parseMultiArgs(optarg, a, 2);
+                switch (status){
+                    case ok:
+                        cout << "Option -b with values " << a[0] << " and " << a[1] << endl;
+                        if ( !(options->setBg(true, a[0], a[1])) )
+                            status = error;
+                        break;
+                    case warning:
+                        cout << "Warning: option -b with values " << optarg << endl;
+                        if ( !(options->setBg(true, a[0], a[1])) )
+                            status = error;
+                        break;
+                    case error:
+                        cout << "Error: option -b with values " << optarg << endl;
+                }
+                break;
+            }
+
+            case 's': {
+                int a[2] = {0};
+                Status status = parseMultiArgs(optarg, a, 4);
+                switch (status){
+                    case ok:
+                        cout << "Option -s with values " << a[0] << " and " << a[1] << endl;
+                        if ( !(options->setSBg(true, a[0], a[1], a[2], a[3])) )
+                            status = error;
+                        break;
+                    case warning:
+                        cout << "Warning: option -s with values " << optarg << endl;
+                        if ( !(options->setSBg(true, a[0], a[1], a[2], a[3])) )
+                            status = error;
+                        break;
+                    case error:
+                        cout << "Error: option -s with values " << optarg << endl;
+                }
+                break;
+            }
+            
+            case 'z': {
+                cout << "Option -z " << endl;
+                options->setZeroSup(true);
+                break;
+            }
+
+            case '?':
+            /* getopt_long already printed an error message. */
+            break;
+
+            default:
+            abort ();
+            }
+        }
+
+    /* Print any remaining command line arguments (not options). */
+    if (optind < argc) {
+        printf ("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf ("%s ", argv[optind++]);
+        putchar ('\n');
+    }
+
+    exit (0);
 }
